4a9fb578865f1363d5bc26456bbda6ef31b8d130
bobbywlf <discord.bobbywolf@gmail.com>
2023-05-19 19:56:16 +0200

    Initial commit

---
+++ src/main/java/com/bobbytbh/ineedmoreqol/item/ModItems.java
@@ -0,0 +1,13 @@
+package com.bobbytbh.ineedmoreqol.item;
+
+import com.bobbytbh.ineedmoreqol.ineedmoreqol;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class ModItems {
+
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, ineedmoreqol.MOD_ID);
+
+}
